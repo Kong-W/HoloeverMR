85056725d56507582e5ee2213fd543d50d2044d7
kongwei <835475119@qq.com>
2021-05-14 17:21:22 +0800

    修改导入包，添加 plugins

--- 发布UPM.cmd
+++ 发布UPM.cmd
@@ -1,7 +1,7 @@
 ::设置模块名字,发布的时候 改路径，名字 版本
-SET ToolName=UPM-HoloeverXR-1_0_3
+SET ToolName=UPM-HoloeverXR-1_0_4
 ::设置模块版本
-SET ToolVersion=1.0.3
+SET ToolVersion=1.0.4
 ::设置模块源路径
 SET ToolAssetPath=Assets/
 
